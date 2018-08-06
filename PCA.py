ce13d904811284d44df4d55890bbdd0125851195
hodayamar <hodayamar2000@gmail.com>
2018-08-06 18:11:38 +0300

    Several attempts to increase accuracy by fine tuning and transfer learning reached 65% and 78% accuracy respectively.

--- PCA.py
+++ PCA.py
@@ -1,130 +1,82 @@
 
-
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+import tensorflow
+import numpy as np
+from matplotlib import pyplot
 import pandas as pd
-import cv2
-from cv2 import *
-import numpy as np
-from scipy.stats import wasserstein_distance
-from matplotlib import pyplot as plt
-import matplotlib.pyplot as plt
-from matplotlib.legend_handler \
-    import HandlerLine2D
-import matplotlib.patches as mpatches
-from time import time
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import metrics
-from sklearn.cluster import KMeans
-from sklearn.datasets import load_digits
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.datasets import make_regression
+from sklearn.preprocessing import MinMaxScaler
+
+from keras.applications.vgg19 import VGG19
+# from keras.applications.densenet.DenseNet169 import DenseNet169
+from keras_applications import densenet
+
 from sklearn.decomposition import PCA
-from skimage import feature
-from skimage import feature
-from sklearn.preprocessing import scale
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
 from matplotlib import pyplot
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
-from skimage.feature import hog
-from skimage import data, exposure
-import random
-def CALC_SHOW_PCA_2D(X,STRING_MODEL):
+from keras import applications
 
-    pca = PCA(n_components=2)
-    X_r = pca.fit(X).transform(X)
-    target_names = ['MRI', 'CT']
-    y = np.full((len(ImagesOfMri)), 0)
-    y1 = np.full((len(ImagesOfCt)), 1)
-    y = np.concatenate((y, y1), axis=0, out=None)
 
-    # Percentage of variance explained for each components
-    print('explained variance ratio (first two components): %s'
-          % str(pca.explained_variance_ratio_))
 
-    plt.figure()
-    colors = ['navy', 'turquoise']
-    lw = 2
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input
+import numpy as np
 
-    for color, i, target_name in zip(colors, [0, 1], target_names):
-        plt.scatter(X_r[y == i, 0], X_r[y == i, 1], color=color, alpha=.8, lw=lw,
-                    label=target_name)
-    plt.legend(loc='best', shadow=False, scatterpoints=1)
-    plt.title('PCA of mri/ct '+STRING_MODEL)
-    plt.show()
 
-def CALC_SHOW_PCA_3D(Data, STRING_MODEL):
-    pca = PCA(n_components=3)
-    DataPCA = pca.fit(Data).transform(Data)
-    target_names = ['MRI', 'CT']
-    group1 = np.full((len(ImagesOfMri)), 0)#Mri Group=0
-    group2 = np.full((len(ImagesOfCt)), 1)#ct Group=0
-    Group = np.concatenate((group1, group2), axis=0, out=None)#all Groups
-    x = [x for (x ,y , z) in DataPCA]
-    y = [y for (x ,y , z) in DataPCA]
-    z=[z for (x ,y , z) in DataPCA]
-
-    fig = pyplot.figure()
-    ax = Axes3D(fig)
-    colors = ['red', 'blue']
-    lw = 2
-
-    for color, i, target_name in zip(colors, [0, 1], target_names):#Draw a 3D graph by the colored groups
-      ax.scatter(DataPCA[Group == i, 0], DataPCA[Group == i, 1], DataPCA[Group == i, 2], color=color, alpha=.8, lw=lw, label=target_name)
-    plt.legend(loc='best', shadow=False, scatterpoints=1)
-    plt.title('PCA of mri/ct '+STRING_MODEL)
-
-    pyplot.show()
-
-def calc_dsy(images):
-    img_to_2d= [two_dim(im) for im in images]
-    img_to_dsy = [skimage.feature.daisy(img, step=15, radius=15, rings=3, histograms=8,
-                          orientations=4, normalization='l1', sigmas=None, ring_radii=None, visualize=False_) for img in img_to_2d]
-    return img_to_dsy
-
-def img_to_histogram(images):
-    # create an array of histogram for all the images
-    img_to_hist = [cv2.calcHist([img], [0], None, [256], [0, 256]) for img in images]
-    # Calculation of a normalized histogram
-    img_to_hist = [norm_hist(hist) for hist in img_to_hist]
-    return img_to_hist
-# def HOG(images):
-#     hog_image =[feature.hog(img, orientations=9, pixels_per_cell=(8, 8),
-# 	cells_per_block=(2, 2), transform_sqrt=True, block_norm="L1") for img in images]
+# def CALC_SHOW_PCA_3D(Data, STRING_MODEL):
+#     pca = PCA(n_components=3)
+#     DataPCA = pca.fit(Data).transform(Data)
+#     target_names = ['MRI', 'CT']
+#     group1 = np.full((len(ImagesOfMri)), 0)#Mri Group=0
+#     group2 = np.full((len(ImagesOfCt)), 1)#ct Group=0
+#     Group = np.concatenate((group1, group2), axis=0, out=None)#all Groups
+#     x = [x for (x ,y , z) in DataPCA]
+#     y = [y for (x ,y , z) in DataPCA]
+#     z=[z for (x ,y , z) in DataPCA]
 #
-#     print(hog_image)
-#     return hog_image
+#     fig = pyplot.figure()
+#     ax = Axes3D(fig)
+#     colors = ['red', 'blue']
+#     lw = 2
+#
+#     for color, i, target_name in zip(colors, [0, 1], target_names):#Draw a 3D graph by the colored groups
+#       ax.scatter(DataPCA[Group == i, 0], DataPCA[Group == i, 1], DataPCA[Group == i, 2], color=color, alpha=.8, lw=lw, label=target_name)
+#     plt.legend(loc='best', shadow=False, scatterpoints=1)
+#     plt.title('PCA of mri/ct '+STRING_MODEL)
+#
+#     pyplot.show()
 
-def norm_hist(hist):
-    img_to_hist = cv2.normalize(hist, hist)
-    return img_to_hist
+# model = VGG16(weights='imagenet', include_top=False)
+# model = ResNet50(weights='imagenet')
+# model = VGG19(weights='imagenet')
+np.random.seed(7)
 
-
-def calc_avg_hist(array_of_hist):
-    """The function get a list names of relevant images end return tha avarage histogram for all images"""
-
-    # convert list to an array
-    a = np.array(array_of_hist)
-    # Calculation of average histogram
-    avg_hist = np.mean(a, axis=0)
-    return avg_hist
-
-
-def two_dim(array):
-    """"The function accepts an array with more than two dimensions and returns an array with one dimension"""
-    two_dim = [hist.flatten() for hist in array]
-    return two_dim
+model = densenet.DenseNet169(weights='imagenet')
 
 
-def concatenate_mri_ct(mri_arr, ct_arr):
-    return np.concatenate((mri_arr, ct_arr), axis=0, out=None)
+def get_features_images(images):
+    features=[]
+    for img in images:
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        features.append(model.predict(x)[0])
+    return features
 
 
-def calc_canny(array_of_img):
-    img_to_canny = [cv2.Canny(im, 1, 30, L2gradient=False) for im in array_of_img]
-    return img_to_canny
 
 
 df = pd.read_csv('InputFiles/dataset.csv', names=['Images', 'Questions', 'Answers'])  # open csv file and rename columns
+# predictions = pd.read_csv('InputFiles/VQAM.csv', names=['Images', 'Questions', 'Answers'])  # open csv file and rename columns
+
 
 # # dictionary of replaceable words
 replace_dict = {"magnetic resonance imaging": "mri",
@@ -140,34 +92,77 @@ replace_dict = {"magnetic resonance imaging": "mri",
                 }
 df.replace(to_replace=replace_dict, inplace=True, regex=True)  # replace word
 
+
+
 ImagesOfMri = df[(~df['Questions'].str.contains('mri|ct') & df['Questions'].str.contains('what') & df[
     'Answers'].str.contains('mri')) == True]['Images']
 ImagesOfCt = df[(~df['Questions'].str.contains('mri|ct') & df['Questions'].str.contains('what') & df[
     'Answers'].str.contains('ct')) == True]['Images']
 
-ImagesOfMri = [cv2.imread("images\Train-images\\" + img + ".jpg") for img in ImagesOfMri]
-ImagesOfCt = [cv2.imread("images\Train-images\\" + img + ".jpg") for img in ImagesOfCt]
+ImagesMri = [image.load_img("images\Train-images\\" + img + ".jpg", target_size=(224, 224)) for img in ImagesOfMri]
+ImagesCt = [image.load_img("images\Train-images\\" + img + ".jpg", target_size=(224, 224)) for img in ImagesOfCt]
 
-# loading all the image that it's answers contain mri or ct
 
-# *************HISTOGRAM********************
-mri_hist = two_dim(img_to_histogram(ImagesOfMri))
-ct_hist = two_dim(img_to_histogram(ImagesOfCt))
 
-data_hist = concatenate_mri_ct(mri_hist, ct_hist)
 
-CALC_SHOW_PCA_3D(data_hist, "HISTOGRAM")
+featuresMri=get_features_images(ImagesMri)
+featuresCt=get_features_images(ImagesCt)
 
-# *************HOG********************
-# HOG(ImagesOfMri)
+sizeMri=len(featuresMri)
+sizeCt=len(featuresCt)
 
-# *******************DAISY**************************
 
-# mri_dsy=two_dim(calc_dsy(ImagesOfMri[0]))
+TrainingData = np.concatenate((featuresMri[:int(sizeMri*0.8)], featuresCt[:int(sizeCt*0.8)]), axis=0, out=None)
+y = np.full((len(featuresMri[:int(sizeMri*0.8)])), 0)
+y1 = np.full((len(featuresCt[:int(sizeCt*0.8)])), 1)
+Y = np.concatenate((y, y1), axis=0, out=None)
+# print(len(featuresCt))
+
+
+
+
+
+
+sizefeatur=len(featuresCt[0])
+
+# create model
+model = Sequential()
+model.add(Dense(12, input_dim=sizefeatur, init='uniform', activation='relu'))
+model.add(Dense(sizefeatur, init='uniform', activation='relu'))
+model.add(Dense(1, init='uniform', activation='sigmoid'))
+# Compile model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Fit the model-The training proces
+model.fit(TrainingData, Y, epochs=150, batch_size=10,  verbose=2)
+# calculate predictions
+predictions = model.predict(TrainingData)
+# round predictions
+rounded = [round(x[0]) for x in predictions]
+print(rounded)
+
+
+y = np.full((len(featuresMri[int(sizeMri*0.8):])), 0)
+y1 = np.full((len(featuresCt[int(sizeCt*0.8):])), 1)
+Yreal= np.concatenate((y, y1), axis=0, out=None)
+
+# new instances where we do not know the answer
+newData= np.concatenate((featuresMri[int(sizeMri*0.8):], featuresCt[int(sizeCt*0.8):]), axis=0, out=None)
+
+# make a prediction-predict the class
+ynew = model.predict_classes(newData)
+# show the inputs and predicted outputs
 #
-# ct_dsy=two_dim(calc_dsy(ImagesOfCt[0]))
-#
-# data_dsy=concatenate_mri_ct(mri_dsy,ct_dsy)
+for i in range(len(newData)):
+   # print("X=%s, Predicted=%s" % (newData[i], ynew[i]))
+   print("Real=%s,Predicted=%s" % (Yreal[i], ynew[i]))
+    # if Yreal[i]==ynew[i]:
+    #     n+=1
+n=[1 for i in range(len(newData)) if Yreal[i]==ynew[i]]
+correct=sum(n)
+print("%s /%s Are correct "%(correct,len(newData)))
 
-# CALC_SHOW_PCA(data_dsy,"daisy")
+model = applications.VGG16(include_top=False, weights='imagenet')
 
+
+
+# CALC_SHOW_PCA_3D(dataFeatures,"DL-VGG16")
