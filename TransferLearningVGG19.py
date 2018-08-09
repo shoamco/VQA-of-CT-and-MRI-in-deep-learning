45477691e80603af012a4272b9d38e512fa8c9ec
shoamco <shoamc98@gmail.com>
2018-08-09 11:25:42 +0300

    transfer learning in vgg19 model

--- TransferLearningVGG19.py
+++ TransferLearningVGG19.py
@@ -15,16 +15,16 @@ validation_data_dir = "data/valid"
 # test_folder = dataset_folder_path + '/test'
 
 
-
-test_files = glob.glob(validation_data_dir + '/**/*.jpg')
 train_files = glob.glob(train_data_dir + '/**/*.jpg')
+test_files = glob.glob(validation_data_dir + '/**/*.jpg')
 
-nb_train_samples = len(train_data_dir)
-nb_validation_samples = len(validation_data_dir)
+
+nb_train_samples = len(train_files)
+nb_validation_samples = len(test_files)
 print("Number of train examples: " , nb_train_samples)
 print("Number of test examples: ", nb_validation_samples)
 channels = 3
-batch_size = 10
+batch_size = nb_validation_samples
 epochs = 1
 
 model = applications.VGG19(weights = "imagenet", include_top=False, input_shape = (img_width, img_height, channels))
@@ -39,7 +39,7 @@ x = model.output
 x = Flatten()(x)
 x = Dense(1024, activation="relu")(x)
 x = Dropout(0.5)(x)
-x = Dense(1024, activation="relu")(x)
+# x = Dense(1024, activation="relu")(x)
 predictions = Dense(2, activation="softmax")(x)
 
 # creating the final model
@@ -79,34 +79,44 @@ target_size = (img_height, img_width),
 class_mode = "categorical")
 
 # Save the model according to the conditions
-checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
+checkpoint = ModelCheckpoint("vgg19_1.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
 early = EarlyStopping(monitor='val_acc', min_delta=0, patience=10, verbose=1, mode='auto')
 
 
 # Train the model
+# model_final.fit_generator(
+# train_generator,
+# samples_per_epoch = nb_train_samples,
+# epochs = epochs,
+# validation_data = validation_generator,
+# nb_val_samples = nb_validation_samples,
+# callbacks = [checkpoint, early])
+
+
 model_final.fit_generator(
-train_generator,
-samples_per_epoch = nb_train_samples,
-epochs = epochs,
-validation_data = validation_generator,
-nb_val_samples = nb_validation_samples,
-callbacks = [checkpoint, early])
-
-
+    train_generator,
+    steps_per_epoch=train_generator.samples / train_generator.batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.samples / validation_generator.batch_size,
+    verbose=1)
+model_final.summary()
 
 y_pred = model.predict_generator(validation_generator, nb_validation_samples//batch_size, workers=4)
 print(len(y_pred))
 print(y_pred)
+print(len(y_pred[0]))
+print(type(y_pred[0]))
 # model.predict_classes(test_x)
 # np.count_nonzero(y_pred == test_y)/len(test_y)
 
-correct = 0
-for i, f in enumerate(validation_generator.filenames):
-    print(i)
-    # TODO if [0]>[1]
-    if f.startswith('ct') and y_pred[i-2][0]>y_pred[i-2][1]:
-        correct +=1
-    if f.startswith('mri') and y_pred[i-2][1]>=y_pred[i-2][0]:
-        correct +=1
-
-print('Correct predictions: '+str(correct/len(validation_generator.filenames)) , ", num of images: " , len(validation_generator.filenames))
+# correct = 0
+# for i, f in enumerate(validation_generator.filenames):
+#     print(i)
+#     # TODO if [0]>[1]
+#     if f.startswith('ct') and y_pred[i-2][0]>y_pred[i-2][1]:
+#         correct +=1
+#     if f.startswith('mri') and y_pred[i-2][1]>=y_pred[i-2][0]:
+#         correct +=1
+#
+# print('Correct predictions: '+str(correct/len(validation_generator.filenames)) , ", num of images: " , len(validation_generator.filenames))
